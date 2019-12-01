ee96933e5e2f188538d6e5bd84c5c4d6ede61333
Stefan Bjornander <stefan.bjornander@chasacademy.se>
2019-12-01 19:44:47 +0100

    Implemented my_strlen function

--- string.c
+++ string.c
@@ -1,32 +1,27 @@
 #include <stdio.h>
 
-int my_strlen(char *s) // Hello Next
+int my_strlen(char *s)
 {
-<<<<<<< HEAD
-   char *p = s; // Hello, again
-
-while (*p)
-=======
    char *p = s;
-while (*p) // Next Hello
->>>>>>> 510ba580431c49761ad83d4db5c242aa124e6bb6
-      ++p; // Another Hello
 
-return (p - s);
+   while (*p)
+      ++p;
+
+   return (p - s);
 }
 
 int main(void)
 {
    int i;
-char *s[] =
+   char *s[] = 
    {
       "Git tutorials",
       "Tutorials Point"
    };
 
-for (i = 0; i < 2; ++i)
+   for (i = 0; i < 2; ++i)
       
    printf("string lenght of %s = %d\n", s[i], my_strlen(s[i]));
 
-return 0;
+   return 0;
 }
